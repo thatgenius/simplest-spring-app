9b69a21c73427cf35cf8ce3faadd059d36c1e7e2
e.zhbanov <e.zhbanov@i-sys.ru>
2019-03-12 09:28:55 +0500

    add application properties

--- src/main/java/controller/SimplestController.java
+++ src/main/java/controller/SimplestController.java
@@ -1,16 +1,22 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
+@ImportResource("classpath:/config.xml")
 public class SimplestController {
+
+    @Value("${value}")
+    private String value;
 
     @RequestMapping(value = "/")
     public String list(ModelMap model) {
-        model.addAttribute("firstVal", "pretty simple");
+        model.addAttribute("firstVal", value);
         return "main";
     }
 }
